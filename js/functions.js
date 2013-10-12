8477bccd7aee77dc580579e4d337a28e91d46f31
Hovhannes Sargsyan <sarhov@sarhov.com>
2013-10-12 20:44:18 +0400

    the top nav menu enad search block END

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($) {
+
+	// open the search
+	var sBox = $('#search-box');
+	var opensBox = $('#open-search');
+	sBox.hide();
+	opensBox.click(function() {
+		sBox.slideToggle('fast');
+		return false;
+	});	
 	
 });
